2af7944d63613aff5e3baa6bb160a6413e526de5
mbroons <mbrooons@gmail.com>
2017-12-09 17:28:14 +0100

    Added best fit algorithm

--- src/test/java/com/biffbangpow/xspeedit/FirstFitRobotTest.java
+++ src/test/java/com/biffbangpow/xspeedit/FirstFitRobotTest.java
@@ -9,7 +9,7 @@ public class FirstFitRobotTest {
     private FirstFitRobot robot = new FirstFitRobot();
 
     @Test
-    public void test_ff_robot_conforms_to_spec() {
+    public void test_first_fit_robot_conforms_to_spec() {
         // Not bad but not really what is expected: 163/82/46/19/8/55/73/7
         assertEquals(robot.pack("163841689525773"), "163/46/82/55/73/81/9/7");
     }
