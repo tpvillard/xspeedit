515e3ad0170c1226acd2fa828d3fd700cb96e6d4
mbroons <mbrooons@gmail.com>
2017-12-21 22:34:14 +0100

    Fixed unit tests

--- src/test/java/com/biffbangpow/xspeedit/strategy/BestFitSearchTest.java
+++ src/test/java/com/biffbangpow/xspeedit/strategy/BestFitSearchTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class BestFitSearchTest {
 
     @Test
-    public void test_best_fit_is_returned() {
+    public void should_return_8_box_for_item_2() {
 
         BestFitSearch search = new BestFitSearch();
         List<Box> openedBoxes = new ArrayList<>();
@@ -24,5 +24,39 @@ public class BestFitSearchTest {
         openedBoxes.add(second);
 
         Assert.assertEquals(search.searchBox(2, openedBoxes), second);
+    }
+
+    @Test
+    public void should_return_6_box_for_item_2() {
+
+        BestFitSearch search = new BestFitSearch();
+        List<Box> openedBoxes = new ArrayList<>();
+
+        Box first = new Box();
+        first.add(4);
+        openedBoxes.add(first);
+
+        Box second = new Box();
+        second.add(6);
+        openedBoxes.add(second);
+
+        Assert.assertEquals(search.searchBox(2, openedBoxes), second);
+    }
+
+    @Test
+    public void should_return_null_for_item_9() {
+
+        BestFitSearch search = new BestFitSearch();
+        List<Box> openedBoxes = new ArrayList<>();
+
+        Box first = new Box();
+        first.add(4);
+        openedBoxes.add(first);
+
+        Box second = new Box();
+        second.add(6);
+        openedBoxes.add(second);
+
+        Assert.assertEquals(search.searchBox(9, openedBoxes), null);
     }
 }
